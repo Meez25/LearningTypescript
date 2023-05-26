41e421305a07d7a492eb14b0818a235f97be6d13
Yann BRONNER <yann.bronner@gmail.com>
2023-05-26 23:26:38 +0200

    type guard (if "nanana" in variable)

--- app.js
+++ app.js
@@ -10,3 +10,19 @@ function addibou(a, b) {
     }
     return a + b;
 }
+function printEmployeeInformation(emp) {
+    console.log("Here is my name : " + emp.name);
+    if ('privileges' in emp) {
+        console.log("Here is my privilege : " + emp.privileges);
+    }
+}
+let admin1 = {
+    privileges: ["kick ass"],
+    name: "DeadPool",
+};
+let employee1 = {
+    name: "Boring Employee",
+    startDate: new Date(),
+};
+printEmployeeInformation(admin1);
+printEmployeeInformation(employee1);
--- app.ts
+++ app.ts
@@ -19,15 +19,36 @@ const e1: ElevatedEmployee = {
 // On peut remplacer les type par des interfaces, et dans ce cas, utiliser "interface ElevatedEmployee extends Admin, Employee {}"
 
 
-type Combinable= number | string;
-type Numeric= number | boolean;
+type Combinable = number | string;
+type Numeric = number | boolean;
 
-type Universal= Combinable & Numeric;
+type Universal = Combinable & Numeric;
 
 function addibou(a: Combinable, b: Combinable) {
-  if (typeof a === 'string' || typeof b === 'string'){
+  if (typeof a === 'string' || typeof b === 'string') {
     return a.toString() + b.toString()
   }
   return a + b;
 }
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Here is my name : " + emp.name);
+  if ('privileges' in emp) {
+    console.log("Here is my privilege : " + emp.privileges);
+  }
+}
+
+let admin1: Admin = {
+  privileges: ["kick ass"],
+  name: "DeadPool",
+}
+let employee1: Employee = {
+  name : "Boring Employee",
+  startDate : new Date(),
+}
+
+printEmployeeInformation(admin1);
+printEmployeeInformation(employee1);
 
